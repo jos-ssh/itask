6fff1c326b92274ac89aaaf66dc6bf0ec3703b0b
MeerkatBoss <ivan.solodovnikov@outlook.com>
2024-05-24 19:19:33 +0300

    fix: align physical addresses

--- kmod/acpi/mmio.c
+++ kmod/acpi/mmio.c
@@ -1,9 +1,11 @@
 #include "mmio.h"
 
+#include <cassert>
 #include <inc/lib.h>
 #include <inc/memlayout.h>
 #include <inc/mmu.h>
 #include <inc/types.h>
+#include <stdint.h>
 
 #define MMIO_MAX_HEAP_SIZE HUGE_PAGE_SIZE
 
@@ -15,17 +17,20 @@ static size_t LastSize = 0;
 
 void*
 mmio_map_region(physaddr_t paddr, size_t size) {
-    size_t true_size = ROUNDUP(size, PAGE_SIZE);
+    uintptr_t start = ROUNDDOWN(paddr, PAGE_SIZE);
+    uintptr_t end = ROUNDUP(paddr + size, PAGE_SIZE);
+    size_t true_size = end - start;
+    
     if (MmioHeapSize + true_size >= MMIO_MAX_HEAP_SIZE) {
         cprintf("[%08x: acpid] Out of MMIO memory!\n", thisenv->env_id);
         return NULL;
     }
 
-    int res = sys_map_physical_region(paddr, CURENVID, MmioHeapTop, true_size,
+    int res = sys_map_physical_region(start, CURENVID, MmioHeapTop, true_size,
                                       PROT_R | PROT_CD);
-    if (res < 0) { return NULL; }
+    assert(res >= 0);
 
-    LastRegion = MmioHeapTop;
+    LastRegion = MmioHeapTop + (paddr - start);
     LastSize = size;
 
     // Commit allocation
@@ -41,8 +46,12 @@ mmio_remap_last_region(physaddr_t paddr, void* old_vaddr,
     assert(old_vaddr == LastRegion);
     assert(old_size == LastSize);
 
-    size_t old_true_size = ROUNDUP(old_size, PAGE_SIZE);
-    size_t new_true_size = ROUNDUP(new_size, PAGE_SIZE);
+    uintptr_t start = ROUNDDOWN(paddr, PAGE_SIZE);
+    uintptr_t old_end = ROUNDUP(paddr + old_size, PAGE_SIZE);
+    uintptr_t new_end = ROUNDUP(paddr + new_size, PAGE_SIZE);
+
+    size_t old_true_size = old_end - start;
+    size_t new_true_size = new_end - start;
 
     if (new_true_size <= old_true_size) {
         LastSize = new_size;
@@ -55,9 +64,10 @@ mmio_remap_last_region(physaddr_t paddr, void* old_vaddr,
         return NULL;
     }
 
-    int res = sys_map_physical_region(paddr + old_true_size, CURENVID, MmioHeapTop,
+    int res = sys_map_physical_region(old_end, CURENVID, MmioHeapTop,
                                       size_diff, PROT_R | PROT_CD);
-    if (res < 0) { return NULL; }
+    assert(res >= 0);
+
     LastSize = new_size;
 
     // Commit allocation
