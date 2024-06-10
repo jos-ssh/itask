84c4d1340805f101459ec1b0e4a6cddfe338df37
foxidokun <foxido@foxido.dev>
2024-06-11 00:42:40 +0300

    shitty stolen reproduced code, but feature negotiation complete

--- kmod/net/net.c
+++ kmod/net/net.c
@@ -6,11 +6,12 @@
 #include <inc/lib.h>
 #include <inc/rpc.h>
 #include <inc/kmod/init.h>
+#include <stdint.h>
 #include <string.h>
 #include <fs/pci_classes.h>
 #include <kmod/pci/pci.h>
 #include "virtio.h"
-#include "kern/pcireg.h"
+#include "kern/pci_common.h"
 
 #define UNWRAP(res, line) do { if (res != 0) { panic(line ": %i", res); } } while (0)
 
@@ -20,6 +21,8 @@
 envid_t g_InitdEnvid;
 envid_t g_PcidEnvid;
 bool g_IsNetdInitialized = false;
+
+struct virtio_net_device_t net;
 
 #define VENDOR_ID 0x1AF4
 #define DEVICE_ID 0x1000
@@ -60,15 +63,112 @@ void initialize() {
     g_IsNetdInitialized = true;
 } 
 
+static bool
+is_bar_mmio(uint32_t *base_addrs, size_t bar) {
+    return PCI_BAR_RTE_GET(base_addrs[bar]) == 0;
+}
+
+static bool
+is_bar_64bit(uint32_t *base_addrs, size_t bar) {
+    return (PCI_BAR_RTE_GET(base_addrs[bar]) == 0) &&
+           (PCI_BAR_MMIO_TYPE_GET(base_addrs[bar]) == PCI_BAR_MMIO_TYPE_64BIT);
+}
+
+static uint64_t
+get_bar(uint32_t *base_addrs, size_t bar) {
+    uint64_t addr;
+
+    if (is_bar_mmio(base_addrs, bar)) {
+        addr = base_addrs[bar] & PCI_BAR_MMIO_BA;
+
+        if (is_bar_64bit(base_addrs, bar)) {
+            addr |= ((uint64_t)(base_addrs[bar + 1])) << 32;
+        }
+    } else {
+        // Mask off low 2 bits
+        addr = base_addrs[bar] & -4;
+    }
+
+    return addr;
+}
+
+static void
+setup_queue(struct virtq *queue, volatile struct virtio_pci_common_cfg_t *cfg_header) {
+    cfg_header->queue_select = queue->queue_idx;
+    cfg_header->queue_desc   = get_phys_addr(&queue->desc);
+    cfg_header->queue_avail  = get_phys_addr(&queue->avail);
+    cfg_header->queue_used   = get_phys_addr(&queue->used);
+    cfg_header->queue_enable = 1;
+    cfg_header->queue_size   = VIRTQ_SIZE;
+    queue->log2_size = 6;
+
+    queue->desc_free_count = 1 << queue->log2_size;
+    for (int i = queue->desc_free_count; i > 0; --i) {
+        queue->desc[i - 1].next = queue->desc_first_free;
+        queue->desc_first_free = i - 1;
+    }
+}
+
+
+static void
+parse_common_cfg(volatile struct virtio_pci_common_cfg_t *cfg_header) {
+    // Reset device
+    cfg_header->device_status = 0;
+
+    // Wait until reset completes
+    while (cfg_header->device_status != 0) {
+        asm volatile("pause");
+    }
+
+    // Set ACK bit (we recognised this device)
+    cfg_header->device_status |= VIRTIO_STATUS_ACKNOWLEDGE;
+
+    // Set DRIVER bit (we have driver for this device)
+    cfg_header->device_status |= VIRTIO_STATUS_DRIVER;
+
+    // Accept features
+    // VIRTIO_NET_F_MAC — device can tell mac address
+    // VIRTIO_NET_F_STATUS — device can tell link status
+    for (int i = 0; i < 4; ++i) {
+        cfg_header->device_feature_select = i;
+        (void)cfg_header->device_feature;
+        cfg_header->driver_feature_select = i;
+        if (i == 0) {
+            cfg_header->driver_feature = VIRTIO_NET_F_MAC | VIRTIO_NET_F_STATUS;
+        } else {
+            cfg_header->driver_feature = 0x0;
+        }
+    }
+
+    // Say that we are ready to check featurus
+    cfg_header->device_status |= VIRTIO_STATUS_FEATURES_OK;
+
+    // Check if they are OK with this feature set
+    if (!(cfg_header->device_status & VIRTIO_STATUS_FEATURES_OK)) {
+        cfg_header->device_status |= VIRTIO_STATUS_FAILED;
+        panic("FAILED TO SETUP NETWORK: Feature error");
+    }
+
+    // Config two queues
+    net.sendq.queue_idx = VIRTIO_SENDQ;
+    setup_queue(&net.sendq, cfg_header);
+
+    net.recvq.queue_idx = VIRTIO_RECVQ;
+    setup_queue(&net.recvq, cfg_header);
+
+    // Set DRIVER_OK flag
+    cfg_header->device_status |= VIRTIO_STATUS_DRIVER_OK;
+}
+
 static void
 setup_device(struct PciHeaderGeneral* header) {
     assert(header->header.vendor_id == VENDOR_ID && header->header.device_id == DEVICE_ID);
 
     uint8_t cap_offset = header->capabilities_ptr;
     uint8_t cap_offset_old = cap_offset;
-    // uint64_t notify_cap_size = 0;
-    // uint32_t notify_cap_offset = 0;
-    // uint32_t notify_off_multiplier = 0;
+    uint64_t notify_cap_size = 0;
+    uint32_t notify_cap_offset = 0;
+    uint32_t notify_off_multiplier = 0;
 
     while (cap_offset != 0) {
         struct pci_cap_hdr_t cap_header;
@@ -85,45 +185,47 @@ setup_device(struct PciHeaderGeneral* header) {
         uint64_t notify_reg;
         uint64_t bar_addr;
 
-        volatile struct virtio_pci_common_cfg_t *common_cfg_ptr;
+        struct virtio_pci_common_cfg_t *common_cfg_ptr;
 
         switch (cap_header.type) {
         case VIRTIO_PCI_CAP_COMMON_CFG:
             cprintf("COMMON\n");
-            // bar_addr = get_bar(base_addrs, cap_header.bar);
-            // addr = cap_header.offset + bar_addr;
-            // common_cfg_ptr = (volatile struct virtio_pci_common_cfg_t *)addr;
-            // notify_reg = notify_cap_offset + common_cfg_ptr->queue_notify_off * notify_off_multiplier;
-            // gpu.controlq.notify_reg += notify_reg;
+            bar_addr = get_bar(header->bar, cap_header.bar);
+            addr = cap_header.offset + bar_addr;
+            common_cfg_ptr = (struct virtio_pci_common_cfg_t *)addr;
+            sys_map_physical_region(addr, CURENVID, common_cfg_ptr, sizeof(*common_cfg_ptr), PROT_RW | PROT_CD);
+            notify_reg = notify_cap_offset + common_cfg_ptr->queue_notify_off * notify_off_multiplier;
+            // net.controlq.notify_reg += notify_reg; // ????
 
-            // if (notify_cap_size < common_cfg_ptr->queue_notify_off * notify_off_multiplier + 2) {
-            //     cprintf("Wrong size\n");
-            // }
-            // parse_common_cfg(pcif, common_cfg_ptr);
+            if (notify_cap_size < common_cfg_ptr->queue_notify_off * notify_off_multiplier + 2) {
+                cprintf("Wrong size\n");
+            }
+
+            parse_common_cfg(common_cfg_ptr);
             break;
         case VIRTIO_PCI_CAP_NOTIFY_CFG:
             cprintf("NOTIFY\n");
 
-            // if (notify_cap_size) {
-            //     break;
-            // }
-            // notify_cap_size = cap_header.length;
-            // notify_cap_offset = cap_header.offset;
+            if (notify_cap_size) {
+                break;
+            }
+            notify_cap_size = cap_header.length;
+            notify_cap_offset = cap_header.offset;
 
-            // gpu.controlq.notify_reg += get_bar(base_addrs, cap_header.bar);
+            // net.controlq.notify_reg += get_bar(header->bar, cap_header.bar);
+            // ????
 
-            // pci_memcpy_from(pcif, cap_offset_old + sizeof(cap_header),
-            //                 (uint8_t *)&notify_off_multiplier, sizeof(notify_off_multiplier));
+            notify_off_multiplier = *(uint32_t *)(((char*) header) + cap_offset_old + sizeof(cap_header));
             break;
         case VIRTIO_PCI_CAP_ISR_CFG:
             cprintf("ISR\n");
 
-            // addr = cap_header.offset + get_bar(base_addrs, cap_header.bar);
-            // gpu.isr_status = (uint8_t *)addr;
+            addr = cap_header.offset + get_bar(header->bar, cap_header.bar);
+            net.isr_status = (uint8_t *)addr;
             break;
         case VIRTIO_PCI_CAP_DEVICE_CFG:
             cprintf("DEVICE\n");
-            // gpu.conf = (struct virtio_gpu_config *)(cap_header.offset + get_bar(base_addrs, cap_header.bar));
+            net.conf = (struct virtio_net_config_t *)(cap_header.offset + get_bar(header->bar, cap_header.bar));
             break;
         case VIRTIO_PCI_CAP_PCI_CFG: break;
         default: break;
--- kmod/net/virtio.h
+++ kmod/net/virtio.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <inc/types.h>
-
+#include <stdint.h>
+#include "virtio-queue.h"
 
 #define VIRTIO_STATUS_ACKNOWLEDGE   1
 #define VIRTIO_STATUS_DRIVER        2
@@ -9,6 +10,9 @@
 #define VIRTIO_STATUS_FEATURES_OK   8
 #define VIRTIO_STATUS_NEED_RESET    64
 #define VIRTIO_STATUS_FAILED        128
+
+#define VIRTIO_NET_F_MAC            (1u << 5)
+#define VIRTIO_NET_F_STATUS         (1u << 16)
 
 // Common configuration
 #define VIRTIO_PCI_CAP_COMMON_CFG   1
@@ -25,13 +29,79 @@
 // PCI configuration access
 #define VIRTIO_PCI_CAP_PCI_CFG      5
 
-struct pci_cap_hdr_t {
-    uint8_t cap_vendor;
-    uint8_t cap_next;
-    uint8_t cap_len;
-    uint8_t type;
-    uint8_t bar;
-    uint8_t padding[3];
-    uint32_t offset;
-    uint32_t length;
+struct virtio_pci_common_cfg_t {
+    // About the whole device
+
+    // read-write
+    uint32_t device_feature_select;
+
+    // read-only for driver
+    uint32_t device_feature;
+
+    // read-write
+    uint32_t driver_feature_select;
+
+    // read-write
+    uint32_t driver_feature;
+
+    // read-write
+    uint16_t config_msix_vector;
+
+    // read-only for driver
+    uint16_t num_queues;
+
+    // read-write
+    uint8_t device_status;
+
+    // read-only for driver
+    uint8_t config_generation;
+
+    // About a specific virtqueue
+
+    // read-write
+    uint16_t queue_select;
+
+    // read-write, power of 2, or 0
+    uint16_t queue_size;
+
+    // read-write
+    uint16_t queue_msix_vector;
+
+    // read-write
+    uint16_t queue_enable;
+
+    // read-only for driver
+    uint16_t queue_notify_off;
+
+    // read-write
+    uint64_t queue_desc;
+
+    // read-write
+    uint64_t queue_avail;
+
+    // read-write
+    uint64_t queue_used;
+};
+
+struct virtio_net_config_t {
+    uint8_t mac[6];
+    uint16_t status;
+    uint16_t max_virtqueue_pairs;
+    uint16_t mtu;
+    uint32_t speed;
+    uint8_t duplex;
+    uint8_t rss_max_key_size;
+    uint16_t rss_max_indirection_table_length;
+    uint32_t supported_hash_types;
+};
+
+#define VIRTIO_RECVQ 0
+#define VIRTIO_SENDQ 1
+
+struct virtio_net_device_t {
+    struct virtq sendq;
+    struct virtq recvq;
+
+    uint8_t *isr_status;
+    struct virtio_net_config_t *conf;
 };
