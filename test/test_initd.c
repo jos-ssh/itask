f0107fffea6c74ff5923a4cdb86356c98daf28d4
MeerkatBoss <70385839+MeerkatBoss@users.noreply.github.com>
2024-06-17 23:10:09 +0300

    Enable explicit parent for spawn and fork in initd (#20)

--- test/test_initd.c
+++ test/test_initd.c
@@ -9,97 +9,101 @@ static union InitdRequest request;
 
 void
 check_spawn_date(envid_t initd) {
-  strcpy(request.spawn.file, "/date");
-  request.spawn.argc = 0;
+    request.spawn.parent = 0;
+    strcpy(request.spawn.file, "/date");
+    request.spawn.argc = 0;
 
-  printf("INITD CHECK: spawn date\n");
-  void* res_data = NULL;
-  int res = rpc_execute(initd, INITD_REQ_SPAWN, &request, &res_data);
+    printf("INITD CHECK: spawn date\n");
+    void* res_data = NULL;
+    int res = rpc_execute(initd, INITD_REQ_SPAWN, &request, &res_data);
 
-  if (res < 0) {
-    panic("spawn error: %i", res);
-  }
-  envid_t child = res;
+    if (res < 0) {
+        panic("spawn error: %i", res);
+    }
+    envid_t child = res;
 
-  res = sys_env_set_status(child, ENV_RUNNABLE);
-  if (res < 0) {
-    panic("status error: %i", res);
-  }
+    res = sys_env_set_status(child, ENV_RUNNABLE);
+    if (res < 0) {
+        panic("status error: %i", res);
+    }
 
-  printf("INITD LOG: spawned 'date' as [%08x]\n", child);
-  wait(child);
+    printf("INITD LOG: spawned 'date' as [%08x]\n", child);
+    wait(child);
 };
 
 void
 check_spawn_echo(envid_t initd) {
-  strcpy(request.spawn.file, "/echo");
-  request.spawn.argc = 3;
-  char arg0[] = "echo";
-  char arg1[] = "Hello, ";
-  char arg2[] = "world!";
-  
-  request.spawn.argv[0] = 0;
-  request.spawn.argv[1] = sizeof(arg0);
-  request.spawn.argv[2] = sizeof(arg0) + sizeof(arg1);
+    request.spawn.parent = 0;
+    strcpy(request.spawn.file, "/echo");
+    request.spawn.argc = 3;
+    char arg0[] = "echo";
+    char arg1[] = "Hello, ";
+    char arg2[] = "world!";
 
-  memcpy(request.spawn.strtab, arg0, sizeof(arg0));
-  memcpy(request.spawn.strtab + sizeof(arg0), arg1, sizeof(arg1));
-  memcpy(request.spawn.strtab + sizeof(arg0) + sizeof(arg1), arg2, sizeof(arg2));
+    request.spawn.argv[0] = 0;
+    request.spawn.argv[1] = sizeof(arg0);
+    request.spawn.argv[2] = sizeof(arg0) + sizeof(arg1);
 
-  printf("INITD CHECK: spawn echo\n");
-  void* res_data = NULL;
-  int res = rpc_execute(initd, INITD_REQ_SPAWN, &request, &res_data);
+    memcpy(request.spawn.strtab, arg0, sizeof(arg0));
+    memcpy(request.spawn.strtab + sizeof(arg0), arg1, sizeof(arg1));
+    memcpy(request.spawn.strtab + sizeof(arg0) + sizeof(arg1), arg2, sizeof(arg2));
 
-  if (res < 0) {
-    panic("spawn error: %i", res);
-  }
+    printf("INITD CHECK: spawn echo\n");
+    void* res_data = NULL;
+    int res = rpc_execute(initd, INITD_REQ_SPAWN, &request, &res_data);
 
-  envid_t child = res;
-  res = sys_env_set_status(child, ENV_RUNNABLE);
-  if (res < 0) {
-    panic("status error: %i", res);
-  }
+    if (res < 0) {
+        panic("spawn error: %i", res);
+    }
 
-  printf("INITD LOG: spawned 'echo' as [%08x]\n", child);
-  wait(child);
+    envid_t child = res;
+    res = sys_env_set_status(child, ENV_RUNNABLE);
+    if (res < 0) {
+        panic("status error: %i", res);
+    }
+
+    printf("INITD LOG: spawned 'echo' as [%08x]\n", child);
+    wait(child);
 }
 
 void
 check_fork(envid_t initd) {
-  void* res_data = NULL;
-  printf("INITD CHECK: fork\n");
-  envid_t parent = thisenv->env_id;
+    void* res_data = NULL;
+    printf("INITD CHECK: fork\n");
+    envid_t parent = thisenv->env_id;
 
-  int res = rpc_execute(initd, INITD_REQ_FORK, NULL, &res_data);
-  // fixup thisenv
-  thisenv = &envs[ENVX(sys_getenvid())];
-  res = thisenv->env_ipc_value;
+    request.fork.parent = 0;
 
-  if (res < 0) {
-    panic("spawn error: %i", res);
-  }
-
-  assert(res != sys_getenvid());
-  if (res == 0) {
-    printf("INITD LOG: child running\n");
+    int res = rpc_execute(initd, INITD_REQ_FORK, &request, &res_data);
+    // fixup thisenv
     thisenv = &envs[ENVX(sys_getenvid())];
+    res = thisenv->env_ipc_value;
 
-    assert(thisenv->env_id != parent);
-    assert(thisenv->env_parent_id == parent);
-    assert(thisenv->env_type == ENV_TYPE_USER);
-    return;
-  }
+    if (res < 0) {
+        panic("spawn error: %i", res);
+    }
 
-  envid_t child = res;
-  assert(envs[ENVX(child)].env_ipc_value == 0);
+    assert(res != sys_getenvid());
+    if (res == 0) {
+        printf("INITD LOG: child running\n");
+        thisenv = &envs[ENVX(sys_getenvid())];
 
-  res = sys_env_set_status(child, ENV_RUNNABLE);
-  if (res < 0) {
-    panic("status error: %i", res);
-  }
+        assert(thisenv->env_id != parent);
+        assert(thisenv->env_parent_id == parent);
+        assert(thisenv->env_type == ENV_TYPE_USER);
+        return;
+    }
 
-  printf("INITD LOG: forked child [%08x]\n", child);
-  wait(child);
+    envid_t child = res;
+    assert(envs[ENVX(child)].env_ipc_value == 0);
+
+    res = sys_env_set_status(child, ENV_RUNNABLE);
+    if (res < 0) {
+        panic("status error: %i", res);
+    }
+
+    printf("INITD LOG: forked child [%08x]\n", child);
+    wait(child);
 }
 
 void
