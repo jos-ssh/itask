c60862fdcc7915b389fb714eb57f9089e741722e
Simonaytes <yandexman228@gmail.com>
2024-06-20 05:18:46 +0300

    Add test for getdents

--- user/testdirsupport.c
+++ user/testdirsupport.c
@@ -28,13 +28,13 @@ void test_remove() {
     cprintf("REMOVE TEST: During mkdir \"%s\" <%i>(%d)\n", directory, res, res);
  
     int fd1 = open(file1, O_RDWR | O_CREAT);
-    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file1, fd1, fd1);
-    close(fd1);
     int fd2 = open(file2, O_RDWR | O_CREAT);
-    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file2, fd2, fd2);
-    close(fd2);
     int fd3 = open(file3, O_RDWR | O_CREAT);
+    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file1, fd1, fd1);
+    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file2, fd2, fd2);
     cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file3, fd3, fd3);
+    close(fd1);
+    close(fd2);
     close(fd3);
     
     res = remove(file1);
@@ -53,7 +53,35 @@ void test_remove() {
 }
 
 void test_getdents() {
+    cprintf("\n==========================\nSTART TEST GETDENTS\n==========================\n");
+    int res = 0;
 
+    res = mkdir(directory);
+    cprintf("GETDENTS TEST: During mkdir \"%s\" <%i>(%d)\n", directory, res, res);
+
+    int fd1 = open(file1, O_RDWR | O_CREAT);
+    int fd2 = open(file2, O_RDWR | O_CREAT);
+    int fd3 = open(file3, O_RDWR | O_CREAT);
+    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file1, fd1, fd1);
+    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file2, fd2, fd2);
+    cprintf("REMOVE TEST: open \"%s\" <%i>(%d)\n", file3, fd3, fd3);
+    close(fd1);
+    close(fd2);
+    close(fd3);
+
+    struct File files[3];
+    res = getdents(directory, files, 2);
+    cprintf("REMOVE TEST: getdents \"%s\", 2 <%i>(%d)\n", directory, res, res);
+    cprintf("REMOVE TEST: file[0] = <%s>\n", files[0].f_name);
+    cprintf("REMOVE TEST: file[1] = <%s>\n", files[1].f_name);
+    res = getdents(directory, files, 3);
+    cprintf("REMOVE TEST: getdents \"%s\", 3 <%i>(%d)\n", directory, res, res);
+    cprintf("REMOVE TEST: file[0] = <%s>\n", files[0].f_name);
+    cprintf("REMOVE TEST: file[1] = <%s>\n", files[1].f_name);
+    cprintf("REMOVE TEST: file[1] = <%s>\n", files[2].f_name);
+
+
+    cprintf("\n==========================\nEND TEST GETDENTS\n==========================\n");
 }
 
 void
