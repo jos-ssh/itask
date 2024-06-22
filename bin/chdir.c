ce38be0fff7e946174d5a125befbf6e9851bd0e7
Simonaytes <yandexman228@gmail.com>
2024-06-22 09:05:27 +0300

    Implement chdir

---
+++ bin/chdir.c
@@ -0,0 +1,17 @@
+#include <inc/lib.h>
+
+void
+umain(int argc, char **argv) {
+    binaryname = "chdir";
+
+    if (argc != 2) {
+        printf("chdir: wrong number of arguments\n");
+        return;
+    }
+    printf("<%s>\n", argv[1]);
+
+    int res = set_cwd(argv[1]);
+    if (res < 0) {
+        printf("chdir: error %i(%d)", res, res);
+    }
+}
