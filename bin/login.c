957b263645fbc43f62914ae36b95f56c137c5b22
egorSharapov <sharapov.ev@phystech.edu>
2024-06-26 20:36:42 +0300

    [fsformat] remove sleep in login.c

--- bin/login.c
+++ bin/login.c
@@ -14,15 +14,6 @@ static void help(void);
 static void no_users_login(void);
 static bool login(void);
 
-// Workaround for preventing overlapping of initd output
-static void
-sleep(int secons) {
-    secons *= 1000;
-    while (secons--) {
-        sys_yield();
-    }
-}
-
 void
 umain(int argc, char** argv) {
     int r;
@@ -51,9 +42,6 @@ umain(int argc, char** argv) {
     if (passwd_stat.st_size == 0)
         return no_users_login();
 
-
-    // workaround for prety console print
-    sleep(1);
 
     while (true) {
         size_t attempt = 0;
